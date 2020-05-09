79e40b2e2b3b30dd205b031fc35db79a0b6c20ca
Belianin <belyaninwork@gmail.com>
2020-05-09 19:30:05 +0500

    Pro precision

--- Blocktrader.Api/Controllers/TimestampController.cs
+++ Blocktrader.Api/Controllers/TimestampController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Nexus.Blocktrader.Domain;
+using Nexus.Blocktrader.Service;
 using Nexus.Blocktrader.Service.Files;
 
 namespace Nexus.Blocktrader.Api.Controllers
@@ -33,9 +35,57 @@ namespace Nexus.Blocktrader.Api.Controllers
             if (timestamp.IsFail)
                 return NotFound("Нет такого файла");
 
-            var byteData = timestamp.Value.Select(t => t.ToBytes()).SelectMany(t => t).ToArray();
+            var byteData = timestamp.Value
+                .Select(t => new Timestamp(t.Date, new TickerInfo(
+                    t.TickerInfo.AveragePrice,
+                    new OrderBook(t.TickerInfo.OrderBook.Bids.Flat(1, true).ToArray(),
+                        t.TickerInfo.OrderBook.Asks.Flat(1, false).ToArray()), 
+                    t.TickerInfo.DateTime)))
+                .Select(t => t.ToBytes()).SelectMany(t => t).ToArray();
 
             return File(byteData, "application/btd", "data.btd");
+        }
+    }
+    
+    public static class EnumerableExtensions
+    {
+        private static IEnumerable<Order> Flat(
+            this IEnumerable<Order> orders,
+            Func<Order, float> priceGetter,
+            Func<Order, float, bool> predicate)
+        {
+            // Здесь происходят странные вещества
+            var currentAmount = 0f;
+            var firstOrder = orders.FirstOrDefault();
+            var price = firstOrder != null ? priceGetter(firstOrder) : 0;
+            
+            foreach (var order in orders)
+            {
+                if (predicate(order, price))
+                {
+                    currentAmount += order.Amount;
+                }
+                else
+                {
+                    yield return new Order
+                    {
+                        Amount = currentAmount,
+                        Price = price
+                    };
+                    price = priceGetter(order);
+                    currentAmount = order.Amount;
+                }
+            }
+        }
+        public static IEnumerable<Order> Flat(this IEnumerable<Order> orders, int precision, bool isBid)
+        {
+            if (precision < 0 || orders == null) 
+                return orders;
+            
+            var delta = (float) Math.Pow(10, precision);
+            return isBid
+                ? orders.Flat(o => (float) Math.Floor(o.Price / delta) * delta, (o, p) => p <= o.Price)
+                : orders.Flat(o => (float) Math.Ceiling(o.Price / delta) * delta, (o, p) => p >= o.Price);
         }
     }
 }
